2cefa000ae535998184015a82c30178cd28a4a88
Anton Klysa <antonklysa@gmail.com>
2020-03-25 12:56:12 +0200

    fix: added bridging header * updated version

---
+++ ZeeHomeScreen/ZeeHomeScreen-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ZeeHomeScreen-Bridging-Header.h
+//  ZeeHomeScreen-iOS
+//
+//  Created by Anton Klysa on 25.03.2020.
+//  Copyright © 2020 Miri. All rights reserved.
+//
+
+#ifndef ZeeHomeScreen_Bridging_Header_h
+#define ZeeHomeScreen_Bridging_Header_h
+
+
+#endif /* ZeeHomeScreen_Bridging_Header_h */
