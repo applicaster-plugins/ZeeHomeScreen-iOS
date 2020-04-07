c85230d5ad067e01fb39472c21482a014cfe1e35
MiriVecselboim <m.vicselboim@applicaster.com>
2020-04-07 18:47:32 +0300

    Added the option to present the DS in another plugin

---
+++ ZeeHomeScreen/Classes/Zee5dsAdapter.swift
@@ -0,0 +1,49 @@
+//
+//  Zee5dsAdapter.swift
+//
+//
+//  Created by Miri.
+//  Copyright © 2018 Applicaster. All rights reserved.
+//
+
+import Foundation
+import ApplicasterSDK
+import ZappPlugins
+
+public class Zee5dsAdapter {
+    
+    public static func createScreen(for atomFeed: APAtomFeed?) -> UIViewController {
+        let bundle = Bundle(for: Self.self)
+        let result = SectionCompositeViewController(nibName: "SectionCompositeViewController", bundle: bundle)
+        result.modalPresentationStyle = .fullScreen
+        result.setComponentModel((Zee5dsAdapter.getBaseComponent(for: atomFeed))!)
+        result.atomFeedUrl = atomFeed?.link
+        
+        var config: PluginKeys?
+        var style: PluginKeys?
+        var pluginConfiguration: NSDictionary?
+
+        if let pluginModel = ZPPluginManager.pluginModelById("zee5_home_screen") {
+            pluginConfiguration = pluginModel.configurationJSON
+        }
+        
+        if let screenModel = ZAAppConnector.sharedInstance().genericDelegate.screenModelForPluginID(pluginID: "zee5_home_screen", dataSource: nil) {
+            config = screenModel.general as? PluginKeys ?? PluginKeys()
+            style = screenModel.style?.object as? PluginKeys ?? PluginKeys()
+        }
+        
+        result.screenConfiguration = ScreenConfiguration.init(config: config, style: style, dataSource: atomFeed, configurationJSON: pluginConfiguration)
+        return result
+    }
+    
+    public static func getBaseComponent(for atomFeed: APAtomFeed?) -> ComponentModelProtocol? {
+        if let feed = atomFeed {
+            let model = ComponentModel.init(entry: feed, threshold: 3)
+            model.isVertical = true
+            model.dsUrl = atomFeed?.link
+            return model
+        }
+        return nil
+    }
+    
+}
