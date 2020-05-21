0805aaf0ae21e213b957e521062d496541f72252
Nikolay Smorgun <ns@computools.com>
2020-05-21 17:24:42 +0300

    Update background color for HomeScreenPlugin
    Update background color for HomeScreenPlugin to be match gradient color

--- ZeeHomeScreen/Classes/Components/SectionComposite/SectionCompositeViewController.swift
+++ ZeeHomeScreen/Classes/Components/SectionComposite/SectionCompositeViewController.swift
@@ -332,8 +332,11 @@ import Zee5CoreSDK
                     prepareEPGView()
                 }
             }
+            
         }
 
+        
+        
         collectionView?.collectionViewLayout = collectionFlowLayout()
         self.view.backgroundColor = UIColor.black
         
@@ -713,8 +716,33 @@ import Zee5CoreSDK
       if let color = color, color.isNotEmpty() {
           view.backgroundColor = color
       } else {
-        view.backgroundColor = .black
+        let gradient:CAGradientLayer = CAGradientLayer()
+        gradient.frame.size = self.view.frame.size
+        gradient.colors = [hexStringToUIColor(hex: "#130014").cgColor,hexStringToUIColor(hex: "#2b0225").cgColor] //Or any colors
+        self.view.layer.insertSublayer(gradient, at:0)
       }
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
     // MARK: - Helper functions
