44ab889cb440693dd758495a1fd7611d95017e1c
Atanas Chanev <atanaschanev@Atanass-MacBook-Pro.local>
2020-07-13 15:49:25 +0100

    zna-7525: Reusing banners without generating extra impressions

--- ZeeHomeScreen/Classes/Components/SectionComposite/SectionCompositeViewController.swift
+++ ZeeHomeScreen/Classes/Components/SectionComposite/SectionCompositeViewController.swift
@@ -28,6 +28,7 @@ import Zee5CoreSDK
     @IBOutlet weak var topDistanceConstraint:NSLayoutConstraint?
     
     public var atomFeedUrl: String?
+    private var bannerCellReuseIdentifiers: [String] = []
     
     var screenConfiguration: ScreenConfiguration?
     var userType: UserType?
@@ -565,7 +566,10 @@ import Zee5CoreSDK
             
             if let componentModel = sectionsDataSourceArray[index] as? ComponentModel,
                 let layoutName = componentModel.layoutStyle {
-                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: layoutName, for: indexPath) as? UniversalCollectionViewCell {
+                
+                let reuseIdentifier = (componentModel.type == "BANNER") ? reuseIdentifierForBanner(layoutName: layoutName, index: index) : layoutName
+                
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? UniversalCollectionViewCell {
                     componentModel.screenConfiguration = screenConfiguration
                     cell.backgroundColor = UIColor.clear
                     
@@ -585,6 +589,16 @@ import Zee5CoreSDK
         }
         
         return collectionView.dequeueReusableCell(withReuseIdentifier: "ZeeHomeScreen_Family_Ganges_horizontal_list_1", for: indexPath)
+    }
+    
+    // The banner must be loaded once per cell at position
+    private func reuseIdentifierForBanner(layoutName: String, index: Int) -> String {
+        let reuseIdentifier = layoutName + "_at_" + String(index)
+        if (!bannerCellReuseIdentifiers.contains(reuseIdentifier)) {
+            self.collectionView?.register(UniversalCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+            bannerCellReuseIdentifiers.append(reuseIdentifier)
+        }
+        return reuseIdentifier
     }
     
     // MARK: - UICollectionViewDelegate
